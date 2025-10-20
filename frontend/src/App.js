59bdc9b16a42fe68b0c2a573858701a432485f24
Quynh <quynh222776@student.nctu.edu.vn>
2025-10-20 21:54:46 +0700

    Chỉnh sửa App.js bên backend để tạo conflict

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+// Đây là code phía Backend
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>🚀 Backend cũng chỉnh sửa App.js</h1>
+    </div>
+  );
+}
